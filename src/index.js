39c34d6efba7b940e4eb8a8cd76717dfc30bd1e8
Alex_Malv <OzziOp.absolute@yandex.ru>
2017-12-16 22:03:17 +0300

    configure redux

--- src/index.js
+++ src/index.js
@@ -1,56 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import './style.css';
+import { Provider } from 'react-redux';
+import store from './config/store';
+import App from './components/App';
 
-import Form from './components/Form';
-import Filter from './components/Filter';
-import Table from './components/Table';
-import { getItems } from './apiWrapper';
+import './style.css'
 
-class App extends Component {
-  state = {
-    items: []
-  }
-  constructor() {
-    super();
-    getItems().then((items) => {
-      console.log(items);
-      this.setState({
-        items
-      });
-    });
-  }
-
-  addTask = ({title}) => {
-    this.setState({
-      items: [{
-        id: Date.now(),
-        title,
-        date: new Date()
-      }, ...this.state.items]
-    });
-  }
-
-  toggleChecked = (id) => {
-    this.setState({
-      items: this.state.items.map((item) => (item.id !== id ) ? item : {
-          ...item,
-          done: !item.done
-        }
-      )
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        We have {this.state.items.length} item(s)
-        <Form onAdd={this.addTask}/>
-        <Filter />
-        <Table items={this.state.items} toggleChecked={this.toggleChecked}/>
-      </div>
-    );
-  }
-}
-
-render(<App />, document.getElementById('root'));
+render(
+  <Provider store={store}>
+    <App />
+    </Provider>,
+  document.getElementById("root")
+);
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import { items } from "./items";
+
+const todoApp = combineReducers({items});
+
+export default todoApp;
